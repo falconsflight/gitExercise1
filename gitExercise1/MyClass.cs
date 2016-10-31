f640ae32020fd23c6cf067e68a4eeddaa15cd32d
jacob.edward.falcon <jacob.edward.falcon@eagles.oc.edu>
2016-10-31 13:19:14 -0500

    Added WriteLines in MyClass

--- gitExercise1/MyClass.cs
+++ gitExercise1/MyClass.cs
@@ -11,6 +11,9 @@ namespace gitExercise1
         public static void hello()
         {
             Console.WriteLine("method 'hello' says: hello");
+            Console.WriteLine("This line is from MyClass");
+            Console.WriteLine("This other line is from MyClass");
+            Console.WriteLine("MyClass ending...");
         }
     }
 }
